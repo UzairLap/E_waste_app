c54758acce3be74111d0736991fda6fd0cdfb68e
pravartak01 <pravartak99@gmail.com>
2025-03-18 22:28:57 +0530

    updated

---
+++ lib/widgets/bottom_navbar.dart
@@ -0,0 +1,58 @@
+// import 'package:flutter/material.dart';
+// import '../screens/home_screen.dart';
+// import '../screens/recycle_screen.dart';
+// import '../screens/rewards_screen.dart';
+// import '../screens/learn_screen.dart';
+// import '../screens/profile_screen.dart';
+//
+// class BottomNavBar extends StatefulWidget {
+//   @override
+//   _BottomNavBarState createState() => _BottomNavBarState();
+// }
+//
+// class _BottomNavBarState extends State<BottomNavBar> {
+//   int _selectedIndex = 0;
+//
+//   final List<Widget> _screens = [
+//     HomeScreen(),
+//     RecycleScreen(),
+//     RewardsScreen(),
+//     LearnScreen(),
+//     ProfileScreen(),
+//   ];
+//
+//   void _onItemTapped(int index) {
+//     if (_selectedIndex != index) {
+//       setState(() {
+//         _selectedIndex = index;
+//       });
+//
+//       // Navigate to the selected screen and replace the current one
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => _screens[index]),
+//       );
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _screens[_selectedIndex], // Display the selected screen
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         currentIndex: _selectedIndex,
+//         onTap: _onItemTapped,
+//         selectedItemColor: Colors.green,
+//         unselectedItemColor: Colors.grey,
+//         items: [
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+//           BottomNavigationBarItem(icon: Icon(Icons.location_on), label: "Recycle"),
+//           BottomNavigationBarItem(icon: Icon(Icons.emoji_events), label: "Rewards"),
+//           BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: "Learn"),
+//           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+//         ],
+//       ),
+//     );
+//   }
+// }
